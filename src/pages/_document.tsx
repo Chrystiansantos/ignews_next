0ceab760a28a96af47ca082ea8c98015f00228e1
chrystiansantos <chrystian_santos_silva@hotmail.com>
2021-03-27 16:33:40 -0300

    Condigurando _document font externa

---
+++ src/pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+
+  render() {
+    return (
+      <Html lang="pt-br">
+        <Head>
+          {/* importando a font */}
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
+          <title>IgNews</title>
+        </Head>
+        <body>
+          {/* Main sera todo o conteudo do app */}
+          <Main></Main>
+          {/* Sera onde o next ira colocar os arquivos js */}
+          <NextScript></NextScript>
+        </body>
+      </Html>
+    )
+  }
+
+}
