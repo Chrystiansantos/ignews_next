d859f46538c71adbab3701a46696320af1b432cb
chrystiansantos <chrystian_santos_silva@hotmail.com>
2021-03-27 16:49:12 -0300

    Configurando eslint

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,24 +1,26 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-
+  // "typescript-eslint/explicit-module-boundary-types": "off"
   render() {
     return (
       <Html lang="pt-br">
         <Head>
           {/* importando a font */}
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
+            rel="stylesheet"
+          />
           <title>IgNews</title>
         </Head>
         <body>
           {/* Main sera todo o conteudo do app */}
-          <Main></Main>
+          <Main />
           {/* Sera onde o next ira colocar os arquivos js */}
-          <NextScript></NextScript>
+          <NextScript />
         </body>
       </Html>
-    )
+    );
   }
-
 }
