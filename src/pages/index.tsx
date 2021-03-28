0ec9d8fe7d309dfc7893c6b0ffd6fe08da15bd85
chrystiansantos <chrystian_santos_silva@hotmail.com>
2021-03-28 15:55:18 -0300

    Title dinamico por pagina

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
-import styles from  '../styles/home.module.scss'
+import Head from 'next/head';
+import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Hello world</h1>
+    <>
+      <Head>
+        <title>Inicio | IgNews</title>
+      </Head>
+      <h1 className={styles.title}>Hello world</h1>
+    </>
   );
 }
