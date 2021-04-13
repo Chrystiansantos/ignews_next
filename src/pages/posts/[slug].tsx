bb6b7aa9d261507a87d3a7e6dcc0fb33313c609f
chrystiansantos <chrystian_santos_silva@hotmail.com>
2021-04-13 11:02:53 -0300

    Preview do post

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,10 +1,14 @@
 import { GetServerSideProps } from 'next';
+import { Session } from 'next-auth';
 import { getSession } from 'next-auth/client';
 import Head from 'next/head';
 import { RichText } from 'prismic-dom';
 import { getPrismicClient } from '../../services/prismic';
 import styles from './post.module.scss';
 
+interface ISession extends Session {
+  activeSubscription?: object;
+}
 interface IPostProps {
   post: {
     slug: string;
@@ -38,17 +42,17 @@ export const getServerSideProps: GetServerSideProps = async ({
   req,
   params,
 }) => {
-  const session = await getSession({ req });
+  const session: ISession = await getSession({ req });
+  const { slug } = params;
 
-  if (!session.activeSubscription) {
+  if (!session?.activeSubscription) {
     return {
       redirect: {
-        destination: '/',
+        destination: `/posts/preview/${slug}`,
         permanent: false,
       },
     };
   }
-  const { slug } = params;
   const prismic = getPrismicClient(req);
   const response = await prismic.getByUID('publication', String(slug), {});
   const post = {
